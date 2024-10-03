565b86a4e242098903c618da38f79df7b70e8c9e
JumboEgg <102843250+JumboEgg@users.noreply.github.com>
2024-10-03 14:00:11 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 72.2 MB -BaekjoonHub

--- Java/프로그래머스/2/42842. 카펫/카펫.java
+++ Java/프로그래머스/2/42842. 카펫/카펫.java
@@ -2,43 +2,12 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        ArrayList<Integer> carpetV = getFactor(brown + yellow);
-        ArrayList<Integer> yellowV = getFactor(yellow);
-        
-        for(int i : yellowV) System.out.print(i + " ");
-        
-        int[] answer = null;
-        for(int i=0; i<carpetV.size(); i++) {
-            if(carpetV.get(i) < 3) continue;
-            int carpetH = (brown + yellow) / carpetV.get(i);
-            
-            System.out.println(carpetH + " " + carpetV.get(i));
-            
-            for(int j=0; j<yellowV.size(); j++) {
-                if(yellowV.get(j) > carpetV.get(i) - 2) break;
-                if(carpetV.get(i) - yellowV.get(j) != 2) continue;
-                
-                int yellowH = yellow / yellowV.get(j);
-                
-                System.out.println(yellowH + " " + yellowV.get(j));
-                
-                if(yellowH > carpetH - 2) continue;
-                
-                answer = new int[] {carpetH, carpetV.get(i)};
-            }
-            
-            if(answer != null) break;
-        }
-        
+        int[] answer = getSize(brown, yellow);
         return answer;
     }
     
-    ArrayList<Integer> getFactor(int n) {
-        ArrayList<Integer> factors = new ArrayList<>();
-        for(int i=1; i<=Math.sqrt(n); i++) {
-            if(n%i==0) factors.add(i);
-        }
-        
-        return factors;
+    int[] getSize(int b, int y) {
+        double temp = Math.sqrt(4 - 2*b - 4*y + b*b/4.0);
+        return new int[] {(int)(b/2.0+2+temp)/2, (int)(b/2.0+2-temp)/2};
     }
 }
