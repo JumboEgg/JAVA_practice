713f637e083089bb0f1c8d11250f192cfb9f3db4
JumboEgg <102843250+JumboEgg@users.noreply.github.com>
2025-09-15 19:53:37 +0900

    [Gold II] Title: 문제집, Time: 420 ms, Memory: 42776 KB -BaekjoonHub

--- Java/백준/Gold/1766. 문제집/문제집.java
+++ Java/백준/Gold/1766. 문제집/문제집.java
@@ -3,9 +3,27 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static class P implements Comparable<P> {
+        int num;
+        int prev;
+        List<P> next;
+
+        P(int num) {
+            this.num = num;
+            this.prev = 0;
+            this.next = new ArrayList<>();
+        }
+
+        @Override
+        public int compareTo(P o) {
+            if (this.prev == o.prev) return this.num - o.num;
+            return this.prev - o.prev;
+        }
+    }
+
     static int N, M;
-    static List<Integer>[] graph;
-    static int[] indegree;
+    static P[] ps;
+    static boolean[] solved;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,35 +32,36 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[N+1];
-        indegree = new int[N+1];
-        for (int i=1; i<=N; i++) {
-            graph[i] = new ArrayList<>();
+        ps = new P[N+1];
+        solved = new boolean[N+1];
+
+        Queue<P> problems = new PriorityQueue<>();
+        for(int i=1; i<=N; i++) {
+            ps[i] = new P(i);
         }
 
-        for (int i=0; i<M; i++) {
+        for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            graph[A].add(B);
-            indegree[B]++;
+            ps[B].prev++;
+            ps[A].next.add(ps[B]);
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i=1; i<=N; i++) {
-            if (indegree[i] == 0) pq.offer(i);
+        for(int i=1; i<=N; i++) {
+            if(ps[i].prev > 0) continue;
+            problems.offer(ps[i]);
         }
 
         StringBuilder sb = new StringBuilder();
-        while (!pq.isEmpty()) {
-            int cur = pq.poll();
-            sb.append(cur).append(" ");
-
-            for (int next : graph[cur]) {
-                indegree[next]--;
-                if (indegree[next] == 0) pq.offer(next);
+        while (!problems.isEmpty()) {
+            P cur = problems.poll();
+            for(P p : cur.next) {
+                if(--p.prev == 0) problems.offer(p);
             }
+
+            sb.append(cur.num).append(" ");
         }
 
         System.out.println(sb);
